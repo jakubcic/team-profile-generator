55fd295b949aa7652ea47fb8040408449f2ec2e1
jakub <jakub@uconn.edu>
2022-11-21 18:02:53 -0500

    add writeFile() function, add finish() function, add comments

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const {
 } = require("./lib/questions");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const template = require("./src/template")
 
+// initialize empty array for team data
 const teamArray = [];
 
+// prompt user for manager info, save to teamArray and 
+// proceed to add another team member
 const managerPrompt = async () => {
   let response = await inquirer.prompt(managerQuestions);
   const manager = new Manager(
@@ -20,9 +24,11 @@ const managerPrompt = async () => {
     response.officeNumber
   );
   teamArray.push(manager);
+  console.log(teamArray)
   addTeamMember();
 };
 
+// prompt user to add engineer, intern, or to finish building team
 const addTeamMember = async () => {
   let response = await inquirer.prompt([
     {
@@ -33,15 +39,19 @@ const addTeamMember = async () => {
     },
   ]);
 
+  // call different function depending on user's response
   if (response.option === "Engineer") {
     engineerPrompt();
   } else if (response.option === "Intern") {
     internPrompt();
   } else {
-    writeFile();
+    // finish will take data from teamArray and generate the index.html
+    finish();
   }
 };
 
+// prompt user for engineer info, save to teamArray and 
+// proceed to add another team member
 const engineerPrompt = async () => {
   let response = await inquirer.prompt(engineerQuestions);
   const engineer = new Engineer(
@@ -51,9 +61,12 @@ const engineerPrompt = async () => {
     response.github
   );
   teamArray.push(engineer);
+  console.log(teamArray)
   addTeamMember();
 };
 
+// prompt user for intern info, save to teamArray and 
+// proceed to add another team member
 const internPrompt = async () => {
   let response = await inquirer.prompt(internQuestions);
   const intern = new Intern(
@@ -63,19 +76,37 @@ const internPrompt = async () => {
     response.school
   );
   teamArray.push(intern);
+  console.log(teamArray)
   addTeamMember();
 };
 
-function writeFile() {
-  console.log(teamArray);
-  console.log(teamArray[Engineer]);
-  // insert code here to write to file. pass data from teamArray to
-  // a function that renders the html page by inserting this data
-  // into template literals
+// wrap fs.writeFile in a simpler function
+const writeFile = (fileName, data) => {
+  fs.writeFile(fileName, data, err => 
+    err ? console.error(err) : console.log(`Contents have been written to ${fileName}.`)
+  );
+};
+
+// write data to index.html
+function finish () {
+  // initialize empty string for team member card data
+  let teamCards = "";
+  // loop through teamArray and pass data to renderCard to generate
+  // html for each card, add it to teamCards
+  for (let i = 0; i < teamArray.length; i++) {
+    teamCards = teamCards + template.renderCard(teamArray[i])
+  }
+  // pass html from teamCards to renderPage to insert cards into page
+  let teamPage = template.renderPage(teamCards);
+  // write full html string from teamPage to index.html
+  writeFile('./dist/index.html', teamPage);
 }
 
+// call managerPrompt in init function, run init on script load
 function init() {
+
   managerPrompt();
+
 }
 
 init();
