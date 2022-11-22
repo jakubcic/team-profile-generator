94962e5a9a57f91795937a8a3a1912865a73c05f
jakub <jakub@uconn.edu>
2022-11-21 22:16:06 -0500

    add engineer test suite

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,21 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Engineer", () => {
+  it("should set the github account when assigned", () => {
+    const username = "githubuser";
+    const employee = new Engineer("Jakub", 1234, "email@email.com", username);
+    expect(employee.github).toBe(username);
+  });
+
+  it("should get the Engineer role with getRole() method", () => {
+    const role = "Engineer";
+    const employee = new Engineer("Jakub", 1234, "email@email.com", "jakubcic");
+    expect(employee.getRole()).toBe(role);
+  });
+
+  it("should get the github username with the getGithub() method", () => {
+    const username = "jakubcic";
+    const employee = new Engineer("Jakub", 1234, "email@email.com", username);
+    expect(employee.getGitHub()).toBe(username);
+  });
+});
